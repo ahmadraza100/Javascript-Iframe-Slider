1fe885496cc19aa837231878a8438347ef0a10f1
Ahmad Raza <ahmadrazashafi@gmail.com>
2021-03-16 05:00:09 +0500

    Project Completed

--- app.js
+++ app.js
@@ -1,75 +1,68 @@
 
-var arr = ["https://www.youtube.com/embed/9i1dXa13CxA","https://www.youtube.com/embed/Cl6xlzpURqM",
-"https://www.youtube.com/embed/p5Mu1KnMi-g" , "https://www.youtube.com/embed/S-DBJN2u9wo" , "https://www.youtube.com/embed/VNs_cCtdbPc" , "https://www.youtube.com/embed/3Q6JlEymVRE"
-, "https://www.youtube.com/embed/c8gEQw8sPok" ,"https://www.youtube.com/embed/LD077A_PyIQ"]
+
+
+
+var arr = ["https://www.youtube.com/embed/9i1dXa13CxA", "https://www.youtube.com/embed/Cl6xlzpURqM",
+    "https://www.youtube.com/embed/p5Mu1KnMi-g", "https://www.youtube.com/embed/S-DBJN2u9wo", "https://www.youtube.com/embed/VNs_cCtdbPc", "https://www.youtube.com/embed/3Q6JlEymVRE"
+    , "https://www.youtube.com/embed/c8gEQw8sPok", "https://www.youtube.com/embed/LD077A_PyIQ"]
+
 
 var i = 0;
-document.getElementById("right").addEventListener("click" , ()=>{
-    if(document.getElementById("i").src=arr[i]){
-        i++;
-    document.getElementById("i").style.transform = "translateX(-50vw)";
-    document.getElementById("i").style.opacity = "0";
+
+        console.log(arr.length);
+document.getElementById("right").addEventListener("click",()=>{
+    i++;
     
+    if(arr.length<i){
+        i = 0 ;
+    }
+
+
+    document.getElementById("i").style.transform = "translateX(-50vw)"
+    document.getElementById("i").style.opacity = "0";
+
     window.setTimeout(()=>{
 
-        document.getElementById("i").style.transform = "translateX(100vw)";
+        document.getElementById("i").style.transform = "translateX(100vw)"
+    document.getElementById("i").style.opacity = "0";
         document.getElementById("i").src = arr[i];
 
-        document.getElementById("i").style.opacity = "0";
-    
-    
-    },1200)
-
+    } ,1200 )
     window.setTimeout(()=>{
 
-        document.getElementById("i").style.transform = "translateX(0vw)";
+        document.getElementById("i").style.transform = "translateX(0vw)"
         document.getElementById("i").style.opacity = "1";
-    
-    
-    },1500)
-}
 
-if(i>arr.length-1){
-    i=1;
-    
-}
-document.getElementById("num").innerHTML = `${i+1}/${arr.length}`;
+    } ,1500 )
+
+    document.getElementById("num").innerHTML = `${i}/${arr.length}`
 })
 
-document.getElementById("left").addEventListener("click" , ()=>{
 
 
-    if(document.getElementById("i").src=arr[i]){
-        i= i-1;
-        if(i<0){
-            i=7;
-           
-   
-           }
-    document.getElementById("i").style.transform = "translateX(50vw)";
+
+document.getElementById("left").addEventListener("click",()=>{
+    i--;
+    if(i<0){
+        i = 8;
+    }
+
+    document.getElementById("i").style.transform = "translateX(50vw)"
     document.getElementById("i").style.opacity = "0";
-    
-    
-   
-    window.setTimeout(()=>{
-
-        document.getElementById("i").style.transform = "translateX(-100vw)";
-        document.getElementById("i").src = arr[i];
-
-        document.getElementById("i").style.opacity = "0";
-    
-    
-    },1200)
 
     window.setTimeout(()=>{
 
-        document.getElementById("i").style.transform = "translateX(0vw)";
+        document.getElementById("i").style.transform = "translateX(-100vw)"
+    document.getElementById("i").style.opacity = "0";
+    document.getElementById("i").src = arr[i];
+
+    } ,1200 )
+    window.setTimeout(()=>{
+
+        document.getElementById("i").style.transform = "translateX(0vw)"
         document.getElementById("i").style.opacity = "1";
-        
-        
-    },1500)
-}
-document.getElementById("num").innerHTML = `${i+1}/${arr.length}`;
 
+    } ,1500 )
+
+    document.getElementById("num").innerHTML = `${i}/${arr.length}`
 })
-
